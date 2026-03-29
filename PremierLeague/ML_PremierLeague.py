9e470a89fbe0edd085b3bebb0a5553602077d83d
MiguelBarrera2004 <124256552+MiguelBarrera2004@users.noreply.github.com>
2026-03-29 23:37:59 +0200

    MLPremierLeague

--- PremierLeague/ML_PremierLeague.py
+++ PremierLeague/ML_PremierLeague.py
@@ -40,3 +40,43 @@ df.corr(numeric_only=True)['result_encoded'].sort_values(ascending=False)
 df['HomeGoals'].hist(bins=10)
 plt.title('Distribution of Home Goals')
 plt.show()
+
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.sort_values('Date').reset_index(drop=True)
+
+df['home_goals_avg'] = df.groupby('Home')['HomeGoals'].transform(lambda x: x.rolling(window=5, min_periods=1).mean())
+df['away_goals_avg'] = df.groupby('Away')['AwayGoals'].transform(lambda x: x.rolling(window=5, min_periods=1).mean())
+
+df['home_conceded_avg'] = df.groupby('Home')['AwayGoals'].transform(lambda x: x.rolling(window=5, min_periods=1).mean())
+df['away_conceded_avg'] = df.groupby('Away')['HomeGoals'].transform(lambda x: x.rolling(window=5, min_periods=1).mean())
+
+df = df.dropna()
+
+le_home = LabelEncoder()
+le_away = LabelEncoder()    
+le_result = LabelEncoder()
+
+df['home_encoded'] = le_home.fit_transform(df['Home'])
+df['away_encoded'] = le_away.fit_transform(df['Away'])  
+df['result_encoded'] = le_result.fit_transform(df['result'])
+
+from sklearn.model_selection import train_test_split
+
+features = ['home_encoded', 'away_encoded', 'home_goals_avg', 'away_goals_avg', 'home_conceded_avg', 'away_conceded_avg']
+X = df[features]
+y = df['result_encoded']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+pred_labels = le_result.inverse_transform(predictions[:10])
+print(pred_labels)
+
+
+
